12ab381229cf34b6f1447abc55702905096a13f2
brunoara21 <bruno.rebaza@smoothstack.com>
2021-08-26 16:46:03 -0400

    Initial Spring commit

---
+++ src/main/java/com/ss/scrumptious_customers/ScrumptiousCustomersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ss.scrumptious_customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumptiousCustomersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumptiousCustomersServiceApplication.class, args);
+	}
+
+}
